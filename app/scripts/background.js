02201dce2825602fe34c21f7e75a0e2ea53352af
rkho <rkho@me.com>
2015-07-25 00:03:05 -0700

    Extension checks domain, fires a callback that starts a timer if Slack is the domain

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,6 +4,35 @@ chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+chrome.browserAction.setBadgeText({text: '\'1'});
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+// Listen for contact from contentscript.js
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  // If the message is received, we need to grab the URL of the active tab
+
+  sendResponse({farewell: checkVendor(request.greeting)})
+
+})
+
+function checkVendor(domain) {
+  if (domain === 'slack.com') {
+    return domain;
+  }
+}
+
+function respondToClient() {
+  chrome.tabs.sendMessage(tabs[0].url, {greeting: 'It works!'}, function(response) {
+    console.log(response.farewell);
+  })
+}
+// chrome.tabs.query({currentWindow: true, active:true}, function(tabs) {
+//   chrome.tabs.sendMessage(tabs[0].url, {greeting: 'hello there'}, function(response) {
+//     console.log(response.farewell);
+//   })
+// }
+// });
+
+// chrome.tabs.onRemoved.addListener(function() {
+//   alert('Closed Tab');
+// })
+
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,30 @@
-'use strict';
+// Get the URL for the list:
 
-console.log('\'Allo \'Allo! Content script');
+var url = document.domain;
+
+var sessionInfo = {
+  username: 'Richard',
+  vendor: undefined,
+  duration: 0
+}
+
+chrome.runtime.sendMessage({greeting: url}, function(response) {
+  if (response.farewell === 'slack.com') {
+    sessionInfo.vendor = 'Slack';
+    startTimer();
+    console.log('SLACK');
+  } else {
+    console.log('Not a vendor');
+  }
+})
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log('grettings');
+})
+
+function startTimer() {
+  setInterval(function() {
+    sessionInfo.duration++;
+    console.log(sessionInfo.duration);
+  }, 1000)
+}

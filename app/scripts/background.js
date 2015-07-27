216921fbf87f8f4cacf0d7af6227a19982a0e8cf
rkho <rkho@me.com>
2015-07-26 21:56:57 -0700

    Refactored logic

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,38 +1,73 @@
 'use strict';
 
+// BOILERPLATE
 chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
 chrome.browserAction.setBadgeText({text: '\'1'});
 
-// Listen for contact from contentscript.js
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  // If the message is received, we need to grab the URL of the active tab
+// SESSION CLASS
+var Session = function(username, vendor) {
+  this.username = username,
+  this.vendor = vendor
+  this.duration = 0
+}
 
-  sendResponse({farewell: checkVendor(request.greeting)})
+Session.prototype.startTimer() {
+  setInterval(function() {
+    this.duration++;
+  }, 1000)
+}
 
-})
+Session.prototype.sendToDatabase() {
+  // AJAX POST request to send this to the database
+}
 
+var session;
+
+// TRACKED VENDORS
+var trackedVendors = ['slack.com']
+
+// HELPER METHODS
 function checkVendor(domain) {
-  if (domain === 'slack.com') {
-    return domain;
+  if (trackedVendors.indexOf(domain) !== -1) {
+    session = new Session('Richard', domain);
+    session.startTimer();
   }
 }
 
-function respondToClient() {
-  chrome.tabs.sendMessage(tabs[0].url, {greeting: 'It works!'}, function(response) {
-    console.log(response.farewell);
-  })
-}
-// chrome.tabs.query({currentWindow: true, active:true}, function(tabs) {
-//   chrome.tabs.sendMessage(tabs[0].url, {greeting: 'hello there'}, function(response) {
-//     console.log(response.farewell);
-//   })
-// }
-// });
+// LISTENERS: Communication from contentscript.js
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  // If the message is received, we need to grab the URL of the active tab
+  if (request.greeting) {
+    checkVendor();
+  }
+})
 
-// chrome.tabs.onRemoved.addListener(function() {
-//   alert('Closed Tab');
-// })
+
+
+
+// LISTENERS: Tab Closed
+chrome.tabs.onRemoved.addListener(function() {
+})
+
+
+// LISTENERS: Active Tab
+// When Chrome's active tab changes, fire a callback to:
+  // 1. Store the current sessionInfo into a database
+  // 2. Grab the new domain from the tab
+    // 3. Determine if a new sessionInfo needs to be run if the new tab is a SaaS vendor
+      // 4. If it does, start ti
+      // 5. Otherwise, do nothing, waiting for the next time active tab changes
+
+chrometabs.onActivated.addListener(function(activeInfo) {
+  // Send a POST request to our server, pushing in sessionInfo
+  chrome.tabs.sendMessage(tabs[0].url, {query: 'New domain'}, function(response) {
+    checkVendor();
+  })
+})
+
+// When the current tab closes, fire a callback to:
+  // 1. Store the current sessionInfo into a database
 
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,30 +1,18 @@
-// Get the URL for the list:
-
-var url = document.domain;
-
-var sessionInfo = {
-  username: 'Richard',
-  vendor: undefined,
-  duration: 0
+// GET URL
+function getDomain() {
+  return document.domain;
 }
 
-chrome.runtime.sendMessage({greeting: url}, function(response) {
-  if (response.farewell === 'slack.com') {
-    sessionInfo.vendor = 'Slack';
-    startTimer();
-    console.log('SLACK');
-  } else {
-    console.log('Not a vendor');
+// SENDER: Communicate to background.js
+chrome.runtime.sendMessage({greeting: getDomain()}, function(response) {
+  console.log(response);
+})
+
+// LISTENER: Communication from background.js
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.query === 'New domain') {
+    sendResponse(getDomain())
   }
 })
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  console.log('grettings');
-})
-
-function startTimer() {
-  setInterval(function() {
-    sessionInfo.duration++;
-    console.log(sessionInfo.duration);
-  }, 1000)
-}
+var port = chrome.runtime.connect({data: sessionInfo});
